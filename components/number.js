5212bdcbc7ff496917195fd2a664b3abab4b3c98
pookis <davidaidas@gmail.com>
2020-02-21 10:08:17 +0100

    numbers in row

--- components/number.js
+++ components/number.js
@@ -1,55 +1,46 @@
 import * as React from "react"
-import { StyleSheet, Text, TouchableOpacity } from "react-native"
-import Svg, { Ellipse } from "react-native-svg";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import Svg, { Ellipse } from "react-native-svg"
 
 function Number(props) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.ellipseStack}>
-                <Svg viewBox="0 0 64.00 64.00" style={styles.ellipse}>
-                    <Ellipse
-                        strokeWidth={4}
-                        fill="rgba(175,164,164,1)"
-                        stroke="rgba(230, 230, 230,1)"
-                        cx={32}
-                        cy={32}
-                        rx={30}
-                        ry={30}
-                    ></Ellipse>
-                </Svg>
-                <TextInput placeholder="12" style={styles.textInput}></TextInput>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Svg viewBox="0 0 64.00 64.00" style={styles.ellipse}>
+        <Ellipse
+          strokeWidth={4}
+          fill="rgba(175,164,164,1)"
+          stroke="rgba(230, 230, 230,1)"
+          cx={32}
+          cy={32}
+          rx={30}
+          ry={30}
+        ></Ellipse>
+      </Svg>
+      <Text style={styles.textInput}>{props.nr}</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: 64,
-        height: 64
-    },
-    ellipse: {
-        top: 0,
-        left: 0,
-        width: 64,
-        height: 64,
-        position: "absolute"
-    },
-    textInput: {
-        top: 17,
-        left: 15,
-        width: 33,
-        height: 29,
-        color: "#121212",
-        position: "absolute",
-        fontSize: 30,
-        lineHeight: 0,
-        textAlign: "center"
-    },
-    ellipseStack: {
-        width: 64,
-        height: 64
-    }
-});
+  ellipse: {
+    width: 40,
+    height: 40
+  },
+  textInput: {
+    top: 7,
+    left: 4,
+    width: 33,
+    height: 29,
+    color: "#121212",
+    position: "absolute",
+    fontSize: 22,
+    lineHeight: 0,
+    textAlign: "center"
+  },
+  container: {
+    margin: 3,
+    position: "relative"
+  }
+})
 
-export default Number;
+export default Number
--- screens/SecondScreen.js
+++ screens/SecondScreen.js
@@ -1,15 +1,35 @@
 import * as React from "react"
 import { StyleSheet, View, Text, Button, TouchableOpacity } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
+import Number from "../components/number"
 
 export default function SecondScreen({ navigation }) {
   return (
-    <View>
-      <Text>SECOND SCREEN</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate("Home")}
-      />
+    <View style={styles.container}>
+      <Number nr="3" />
+      <Number nr="26" />
+      <Number nr="30" />
+      <Number nr="4" />
+      <Number nr="16" />
+      <Number nr="30" />
+      <Number nr="3" />
+      <Number nr="26" />
+      <Number nr="30" />
+      <Number nr="4" />
+      <Number nr="16" />
+      <Number nr="30" />
+
+      <Button title="G" onPress={() => navigation.navigate("Home")} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1
+  }
+})
